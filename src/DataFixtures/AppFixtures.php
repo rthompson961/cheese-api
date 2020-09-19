d6aa7b52129d8e6767d88f542f6b57fcb7447987
richard <you@example.com>
2020-09-19 11:41:20 +0100

    fixed issue with fixture text line break conversion

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -13,67 +13,67 @@ class AppFixtures extends Fixture
         $data = [
             [
                 'title' => 'Half eaten blue-cheese',
-                'desc'  => 'it\'s a little\n bit\n whiffy',
+                'desc'  => "it's a little\n bit\n whiffy",
                 'price' => 100,
                 'published' => true
             ],
             [
                 'title' => 'Giant block of cheddar cheese',
-                'desc'  => 'woah\n big block of cheese',
+                'desc'  => "woah\n big block of cheese",
                 'price' => 1000,
                 'published' => true
             ],
             [
                 'title' => 'Delicious Chevre',
-                'desc'  => 'I hade to\n lookup\n chevre',
+                'desc'  => "I hade to\n lookup\n chevre",
                 'price' => 4500,
                 'published' => false
             ],
             [
                 'title' => 'Crumbs of some blue cheese',
-                'desc'  => 'something\n something',
+                'desc'  => "something\n something",
                 'price' => 20,
                 'published' => false
             ],
             [
                 'title' => 'Brie',
-                'desc'  => 'something\n something',
+                'desc'  => "something\n something",
                 'price' => 3500,
                 'published' => true
             ],
             [
                 'title' => 'Red Leicester',
-                'desc'  => 'something\n something',
+                'desc'  => "something\n something",
                 'price' => 600,
                 'published' => true
             ],
             [
                 'title' => 'Camembert',
-                'desc'  => 'something\n something',
+                'desc'  => "something\n something",
                 'price' => 7700,
                 'published' => true
             ],
             [
                 'title' => 'Wensleydale',
-                'desc'  => 'something\n something',
+                'desc'  => "something\n something",
                 'price' => 2250,
                 'published' => true
             ],
             [
                 'title' => 'Edam',
-                'desc'  => 'something\n something',
+                'desc'  => "something\n something",
                 'price' => 3600,
                 'published' => true
             ],
             [
                 'title' => 'Gouda',
-                'desc'  => 'something\n something',
+                'desc'  => "something\n something",
                 'price' => 2800,
                 'published' => true
             ],
             [
                 'title' => 'Parmesan',
-                'desc'  => 'something\n something',
+                'desc'  => "something\n something",
                 'price' => 4200,
                 'published' => true
             ]
