deb5103b8802c5cc5d20a0fdc0bce800a5364225
richard <you@example.com>
2020-09-19 14:34:18 +0100

    added entity relations

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\CheeseListing;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,72 +11,95 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $user1 = new User();
+        $user1->setEmail('cheeselover1@example.com');
+        $user1->setUsername('cheeselover1');
+        $user1->setPassword('pass');
+        $manager->persist($user1);
+
+        $user2 = new User();
+        $user2->setEmail('mrcheese9000@example.com');
+        $user2->setUsername('mrcheese9000');
+        $user2->setPassword('pass');
+        $manager->persist($user2);
+
         $data = [
             [
                 'title' => 'Half eaten blue-cheese',
                 'desc'  => "it's a little\n bit\n whiffy",
                 'price' => 100,
-                'published' => true
+                'published' => true,
+                'owner' => $user1
             ],
             [
                 'title' => 'Giant block of cheddar cheese',
                 'desc'  => "woah\n big block of cheese",
                 'price' => 1000,
-                'published' => true
+                'published' => true,
+                'owner' => $user1
             ],
             [
                 'title' => 'Delicious Chevre',
                 'desc'  => "I hade to\n lookup\n chevre",
                 'price' => 4500,
-                'published' => false
+                'published' => false,
+                'owner' => $user2
             ],
             [
                 'title' => 'Crumbs of some blue cheese',
                 'desc'  => "something\n something",
                 'price' => 20,
-                'published' => false
+                'published' => false,
+                'owner' => $user1
             ],
             [
                 'title' => 'Brie',
                 'desc'  => "something\n something",
                 'price' => 3500,
-                'published' => true
+                'published' => true,
+                'owner' => $user2
             ],
             [
                 'title' => 'Red Leicester',
                 'desc'  => "something\n something",
                 'price' => 600,
-                'published' => true
+                'published' => true,
+                'owner' => $user1
             ],
             [
                 'title' => 'Camembert',
                 'desc'  => "something\n something",
                 'price' => 7700,
-                'published' => true
+                'published' => true,
+                'owner' => $user2
             ],
             [
                 'title' => 'Wensleydale',
                 'desc'  => "something\n something",
                 'price' => 2250,
-                'published' => true
+                'published' => true,
+                'owner' => $user2
             ],
             [
                 'title' => 'Edam',
                 'desc'  => "something\n something",
                 'price' => 3600,
-                'published' => true
+                'published' => true,
+                'owner' => $user1
             ],
             [
                 'title' => 'Gouda',
                 'desc'  => "something\n something",
                 'price' => 2800,
-                'published' => true
+                'published' => true,
+                'owner' => $user1
             ],
             [
                 'title' => 'Parmesan',
                 'desc'  => "something\n something",
                 'price' => 4200,
-                'published' => true
+                'published' => true,
+                'owner' => $user2
             ]
         ];
 
@@ -84,6 +108,7 @@ class AppFixtures extends Fixture
             $cheese->setTextDescription($item['desc']);
             $cheese->setPrice($item['price']);
             $cheese->setIsPublished($item['published']);
+            $cheese->setOwner($item['owner']);
             $manager->persist($cheese);            
         }
 
