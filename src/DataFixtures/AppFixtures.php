ee385c50a81b03270d2b2de2d3e2b62cf16a7f55
richard <you@example.com>
2020-09-20 14:07:06 +0100

    added session based login system

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,21 +6,31 @@ use App\Entity\CheeseListing;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordEncoderInterface $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
     {
         $user1 = new User();
         $user1->setEmail('cheeselover1@example.com');
         $user1->setUsername('cheeselover1');
-        $user1->setPassword('pass');
+        #$user1->setPassword('$argon2i$v=19$m=65536,t=4,p=1$M1czY29NR0haUHhCaVcvbA$w+6MtXwrfsE8sVfZG1LTLrcNITGQX8/F++7mXU5s2bE');
+        $user1->setPassword($this->passwordEncoder->encodePassword($user1, 'pass'));
         $manager->persist($user1);
 
         $user2 = new User();
         $user2->setEmail('mrcheese9000@example.com');
         $user2->setUsername('mrcheese9000');
-        $user2->setPassword('pass');
+        #$user2->setPassword('$argon2i$v=19$m=65536,t=4,p=1$M1czY29NR0haUHhCaVcvbA$w+6MtXwrfsE8sVfZG1LTLrcNITGQX8/F++7mXU5s2bE');
+        $user2->setPassword($this->passwordEncoder->encodePassword($user2, 'pass'));
         $manager->persist($user2);
 
         $data = [
