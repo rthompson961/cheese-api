b7746bd790190ae155015c06247fe3c0f127bda4
richard <you@example.com>
2020-09-20 16:04:38 +0100

    restricted put to listing owner

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -22,7 +22,10 @@ use Symfony\Component\Validator\Constraints as Assert;
  *          "get"={
  *              "normalization_context"={"groups"={"cheese_listing:read", "cheese_listing:item:get"}},
  *          },
- *          "put"={"access_control"="is_granted('ROLE_USER')"},
+ *          "put"={
+ *              "access_control"="is_granted('ROLE_USER') and previous_object.getOwner() == user",
+ *              "access_control_message"="Only the creator can edit a cheese listing"
+ *          },
  *          "delete"={"access_control"="is_granted('ROLE_ADMIN')"}
  *     },
  *    collectionOperations={
